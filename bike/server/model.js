d21caba6f9df806ebf604299ee545d85cfd72332
life96321 <life_96321@sina.com>
2018-05-28 23:19:08 +0800

    后端服务，MongoDB，模拟数据相关
    MongoDB创建

--- bike/server/model.js
+++ bike/server/model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const DB_URL = 'mongodb://localhost:27017/bike'
 mongoose.connect(DB_URL)
 mongoose.connection.on('connected', () => {
-    console.log('connected success')
+    console.log('connected')
 })
 const models = {
     user: {
